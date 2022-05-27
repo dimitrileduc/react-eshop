5aadbb07c98a240e14a3f57cd718bcd5b4beee58
Dimitri Leduc <dimitrileduc@MacBook-Pro-de-Dimitri.local>
2022-05-27 12:20:51 +0200

    formik form and yup js validation

--- src/components/pages/Contacts/styles.js
+++ src/components/pages/Contacts/styles.js
@@ -1,5 +1,54 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    padding-top: 85px;
+export const ContainerPage = styled.div`
+    padding-top: 120px;
+    display: flex;
+    justify-content: center;
+`;
+
+export const ContainerGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto auto 50% auto;
+    gap: 0px 0px;
+    grid-template-areas:
+        "header header"
+        "text text"
+        "content content"
+        "button .";
+`;
+
+export const HeaderContainer = styled.div`
+    justify-self: center;
+    grid-area: header;
+`;
+
+export const TextContainer = styled.div`
+    justify-self: center;
+    grid-area: text;
+`;
+
+export const ContentContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+    gap: 0px 0px;
+    grid-template-areas: "form map";
+    justify-self: center;
+    grid-area: content;
+`;
+
+export const ContainerForm = styled.div`
+    justify-self: end;
+    grid-area: form;
+`;
+
+export const ContainerMap = styled.div`
+    justify-self: start;
+    grid-area: map;
+`;
+
+export const ContainerButton = styled.div`
+    justify-self: start;
+    grid-area: button;
 `;
